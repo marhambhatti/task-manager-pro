f8a36fd2c72d1ad1ad5eb3a6e991d9d86db0f821
Arham Bhatti <muhammadarham0372@gmail.com>
2026-05-24 21:37:55 +0500

    Pagination to Get Task

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -45,6 +45,8 @@ next(error);
 
 }
 }
+
+// Get Task Without Pagination
 const getTask=async (req,res)=>{
     const tasks=await Task.find({
         createdBy:req.user.id
@@ -55,6 +57,29 @@ const getTask=async (req,res)=>{
         tasks
     })
 
+}
+
+// Get Task With Pagination
+
+const getTaskWithPagination=async (req,res,next)=>{
+   try {
+     const page=Number(req.query.page)||1
+    const limit=Number(req.query.limit)||5
+
+    const skip=(page-1)*limit;
+
+    const tasks=await Task.find({
+        createdBy:req.user.id
+    }).skip(skip).limit(limit).sort({createdAt:-1});
+
+    
+    res.status(200).json({
+        success:true,
+        tasks
+    })
+   } catch (error) {
+    next(error);
+   }
 }
 const updateTask=async (req,res,next)=>{
     try{
